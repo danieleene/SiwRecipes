c21b4fa019f71af9f7850540319d36e60116bdcb
danieleene <91674916+danieleene@users.noreply.github.com>
2025-12-23 15:42:17 +0100

    Aggiornata la RicettaController.java
    Sono stati scritti i metodi che offrono:
    
    -La consultazione dei dettagli di una ricetta
    -La lista di tutte le ricette presenti nel sito
    
    Da estendere con nuovi metodi.

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/controller/RicettaController.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/controller/RicettaController.java
@@ -1,5 +1,29 @@
 package it.uniroma3.siw.controller;
 
-public class RicettaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import it.uniroma3.siw.service.RicettaService;
+
+@Controller
+public class RicettaController {
+  
+  @Autowired RicettaService ricettaService;
+	  
+	  @GetMapping("/ricetta/{id}")
+	  public String getRicetta(@PathVariable("id") Long id, Model model) {
+	    model.addAttribute("ricetta", this.ricettaService.getRicettaById(id));
+	    return "ricetta.html";
+	  }
+
+	  @GetMapping("/ricetta")
+	  public String showRicette(Model model) {
+	    model.addAttribute("ricette", this.ricettaService.getAllRicette());
+	    return "ricette.html";
+	  }
+  
 }
+
