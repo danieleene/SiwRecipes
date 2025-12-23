64a5b510bef11323c2bf776a3c0f485ca960f97b
danieleene <91674916+danieleene@users.noreply.github.com>
2025-12-23 15:12:59 +0100

    Update RicettaService.java
    E' stata scritta una semplice classe con poche funzioni offerte sulla classe Ricetta.
    Da estendere!

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/service/RicettaService.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/service/RicettaService.java
@@ -1,5 +1,21 @@
 package it.uniroma3.siw.service;
 
-public class RicettaService {
+import org.springframework.beans.factory.annotation.Autowired;
+import it.uniroma3.siw.model.Ricetta;
+import it.uniroma3.siw.repository.RicettaRepository;
 
+public class RicettaService {
+  
+  @Autowired
+	private RicettaRepository ricettaRepository;
+
+	public Ricetta getRicettaById(Long id) {
+		return ricettaRepository.findById(id).get();
+	}
+
+	public Iterable<Ricetta> getAllRicette() {
+		return ricettaRepository.findAll();
+	}
+  
 }
+
