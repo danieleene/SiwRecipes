6812024a952d48c0e18ec27a67c8a0a00a12015f
danieleene <91674916+danieleene@users.noreply.github.com>
2026-01-21 11:08:27 +0100

    Update UtenteService.java
    In aggiunta è stato implementato solamente il metodo di salvataggio + commenti aggiuntivi

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/service/UtenteService.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/service/UtenteService.java
@@ -11,12 +11,19 @@ public class UtenteService {
 	@Autowired
 	private UtenteRepository utenteRepository;
 
+	//Per ottenere l'utente a partire dal suo ID
 	public Utente getUtenteById(Long id) {
 		return utenteRepository.findById(id).get();
 	}
 
+	//Per ottenere la lista di tutti gli utenti
 	public Iterable<Utente> getAllUtenti() {
 		return utenteRepository.findAll();
+	}
+
+	//Per salvare un utente
+	public Utente saveUtente(Utente utente) {
+		return this.utenteRepository.save(utente);
 	}
 
 	
