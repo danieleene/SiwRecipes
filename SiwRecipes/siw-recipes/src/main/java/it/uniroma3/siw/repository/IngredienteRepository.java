87b26a46b398dae91199a1c55db8ef03c8fb8b63
danieleene <91674916+danieleene@users.noreply.github.com>
2026-01-21 10:49:37 +0100

    E' stato riscritto IngredienteRepository.java
    Da rivedere e correggere però!

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/repository/IngredienteRepository.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/repository/IngredienteRepository.java
@@ -1,9 +1,18 @@
 package it.uniroma3.siw.repository;
 
+import java.util.List;
+
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.model.Ingrediente;
+//import it.uniroma3.siw.model.Ricetta;
 
-public interface IngredienteRepository extends CrudRepository<Ingrediente, String> {
-
+public interface IngredienteRepository extends CrudRepository<Ingrediente, Long> {
+	
+	/* @Query("SELECT i.ricette FROM Ingrediente i WHERE i.id = :ingredienteID")
+	    Iterable<Ricetta> findRicetteByIngredienteID(@Param("ingredienteID") Long ingredienteID);
+*/
+	 List<Ingrediente> findByNomeContainingIgnoreCase(String nome);
 }
