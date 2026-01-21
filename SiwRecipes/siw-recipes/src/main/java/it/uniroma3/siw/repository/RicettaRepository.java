b02c45d10edb49782885087f82caa5ad5238b38c
danieleene <91674916+danieleene@users.noreply.github.com>
2026-01-21 10:51:36 +0100

    Update RicettaRepository.java
    E' stato esteso con il metodo di ricerca delle ricette

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/repository/RicettaRepository.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/repository/RicettaRepository.java
@@ -1,9 +1,17 @@
 package it.uniroma3.siw.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.model.Ricetta;
 
 public interface RicettaRepository extends CrudRepository<Ricetta, Long> {
 
+  @Query("SELECT r FROM Ricetta r WHERE LOWER(r.titolo) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Ricetta> searchRicette(@Param("query") String query);
+
 }
+
