379f9ac97230371de09c18b958017e31b39b67b0
danieleene <91674916+danieleene@users.noreply.github.com>
2026-01-21 10:38:33 +0100

    Cambio fondamentale in Ingrediente.java !
    E' stato aggiunto il campo id all'ingrediente

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/model/Ingrediente.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/model/Ingrediente.java
@@ -2,7 +2,18 @@ package it.uniroma3.siw.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Ingrediente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String nome;
 	private double quantita;
@@ -50,3 +61,4 @@ public class Ingrediente {
 	}
 		
 }
+
