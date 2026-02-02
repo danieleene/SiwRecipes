803961890fea79f2596dab6980b4c8d406f3e1bf
danieleene <91674916+danieleene@users.noreply.github.com>
2026-02-02 15:30:13 +0100

    Aggiunto IMPORT mancante in CredenzialiService.java

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/service/CredenzialiService.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/service/CredenzialiService.java
@@ -1,7 +1,10 @@
 package it.uniroma3.siw.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import it.uniroma3.siw.model.Credenziali;
 import it.uniroma3.siw.repository.CredenzialiRepository;
 
