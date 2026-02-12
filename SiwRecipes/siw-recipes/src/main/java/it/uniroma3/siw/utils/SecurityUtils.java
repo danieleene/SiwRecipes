7a408c876a08875630e8d37b8709c8cd727e5742
danieleene <91674916+danieleene@users.noreply.github.com>
2026-02-12 14:19:48 +0100

    Aggiunti commenti per SecurityUtils.java

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/utils/SecurityUtils.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/utils/SecurityUtils.java
@@ -12,11 +12,13 @@ import it.uniroma3.siw.service.CredenzialiService;
 @Component
 public class SecurityUtils {
 
+    //Verifica se l'utente corrente è autenticato(true, solo se l'utente ha effettuato il login)
     public boolean isAuthenticated() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return auth != null && !(auth instanceof AnonymousAuthenticationToken);
     }
 
+    //Restituisce l'username dell'utente attualmente autenticato
     public String getCurrentUsername() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null && auth.getPrincipal() instanceof UserDetails userDetails) {
@@ -25,11 +27,13 @@ public class SecurityUtils {
         return null;
     }
 
+    //Restituisce le Credenziali(oggetto) dell'utente autenticato
     public Credenziali getCurrentCredentials(CredenzialiService credenzialiService) {
         String username = getCurrentUsername();
         return username != null ? credenzialiService.getCredenziali(username) : null;
     }
-    
+
+    //Verifica del ruolo di ADMIN
     public boolean isAdmin(CredenzialiService credenzialiService) {
     	Credenziali credenziali = getCurrentCredentials(credenzialiService);
     	return credenziali != null && credenziali.getRuolo().equals("ADMIN");
