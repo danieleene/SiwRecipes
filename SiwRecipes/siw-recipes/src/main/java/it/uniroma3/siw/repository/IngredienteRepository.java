4142dac4c5fb26ed1a734da4b1aa4fbfe09fe0fa
danieleene <91674916+danieleene@users.noreply.github.com>
2026-02-14 10:33:15 +0100

    Sistemato IngredienteRepository.java
    Eliminato codice commentato nel Repository di Ingrediente

--- SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/repository/IngredienteRepository.java
+++ SiwRecipes/siw-recipes/src/main/java/it/uniroma3/siw/repository/IngredienteRepository.java
@@ -2,17 +2,13 @@ package it.uniroma3.siw.repository;
 
 import java.util.List;
 
-//import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-//import org.springframework.data.repository.query.Param;
 
 import it.uniroma3.siw.model.Ingrediente;
-//import it.uniroma3.siw.model.Ricetta;
+
 
 public interface IngredienteRepository extends CrudRepository<Ingrediente, Long> {
 	
-	/* @Query("SELECT i.ricette FROM Ingrediente i WHERE i.id = :ingredienteID")
-	    Iterable<Ricetta> findRicetteByIngredienteID(@Param("ingredienteID") Long ingredienteID);
-*/
+
 	 List<Ingrediente> findByNomeContainingIgnoreCase(String nome);
 }
